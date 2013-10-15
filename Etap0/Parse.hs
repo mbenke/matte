8ed368c734f48336ce7579b76c780f06566b6faf
Marcin Benke <ben@mimuw.edu.pl>
2013-10-15 14:04:49 +0200

    Etap0/Parse

---
+++ Etap0/Parse.hs
@@ -0,0 +1,24 @@
+module Parse where
+import Text.Parsec
+import qualified Text.Parsec.Token as PT
+-- import Text.Parsec.Token 
+import Text.Parsec.Language(emptyDef,haskellDef)
+import Control.Applicative((<$>),(<$),(*>),(<*))
+run p = parse (whiteSpace *> p <* eof) "stdin"
+lexer = PT.makeTokenParser lexDef
+lexDef = emptyDef { 
+  PT.reservedOpNames = ["*","/","+","-"]
+  }
+         
+
+integer = Int <$> PT.integer lexer
+operator = op "-" <|> op "+"
+  where op s = Op s <$ PT.reservedOp lexer s
+        
+whiteSpace = PT.whiteSpace lexer
+
+data Item = Int Integer | Op String deriving Show
+
+-- Kind of works but still cannot handle "1 2--3" or even "+-3"
+items  = many item
+item = try integer <|> operator
