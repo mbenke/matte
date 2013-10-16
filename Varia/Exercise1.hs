9c45d1da03874e319c92358595bc772fe94f690d
Marcin Benke <ben@mimuw.edu.pl>
2013-10-16 13:14:19 +0200

    Exercise1: add forgotten part

--- Varia/Exercise1.hs
+++ Varia/Exercise1.hs
@@ -5,8 +5,11 @@ import Control.Applicative(pure,(<$>),(<$),(*>),(<*),(<*>),(<**>))
 import VanillaLexer
 
 test1 = run pE "2*3*4+5+6"
--- >>> run pE "2*3*4+5+6"
+-- >>> test1
 -- Right 40
+test2 = run pE "1*2+3*4+5"
+-- >>> test2
+-- Right 25
 
 run p = parse (spaces *> p <* eof) "stdin"
 
@@ -19,9 +22,9 @@ pE :: Parser T
 pE = pF <**> pE'
 pE' :: Parser (T -> T)
 pE' = e1 <|> e2 where
-  e1 = build <$> (char '*' *> pE) <*> (char '+' *> pF) where
+  e1 = build <$> (char '*' *> pE) <*> (char '+' *> pF) <*> pE' where
   e2 = eps id
-  build e f t = t * e + f
+  build e f g = g . (+f) . (*e) -- (t * e + f)
   
 pF = parens pE <|> integer
 
